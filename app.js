627c65a1401c6db606d8ea3096ba371abd55a420
韦家娥 <weijiae@comba.com.cn>
2018-06-06 11:01:51 +0800

    获取用户基本信息并保存到缓存当中

--- app.js
+++ app.js
@@ -9,32 +9,31 @@ App({
       wx.clearStorageSync();
       wx.setStorageSync('postList', dataObj.postList)
     }
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    this.getUserInfo();
+  },
+  getUserInfo() {
+    // 对用户基本信息使用缓存
+    var userInfoStorage = wx.getStorageSync('user');
+    if (!userInfoStorage) {
+      var that = this;
+      wx.login({
+        success: function () {
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+            success: function (res) {
+              console.log(res);
+              that.globalData.userInfo = res.userInfo
+              wx.setStorageSync('user', res.userInfo)
+            },
+            fail: function (res) {
+              console.log(res);
             }
           })
         }
-      }
-    })
+      })
+    }
+    else {
+      this.globalData.userInfo = userInfoStorage;
+    }
   },
 // 在这里设置全局变量
   globalData: {
