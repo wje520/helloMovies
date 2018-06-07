857fff51a12012c8103959dd91c9769dbaf9b5ac
韦家娥 <weijiae@comba.com.cn>
2018-06-07 16:20:40 +0800

    封装模态框，异步清除本地缓存

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -7,6 +7,16 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 开放接口
+    api: [
+      { title: '清理缓存', tap: 'clearCache' },
+      { title: '二维码', tap: 'scanCode' },
+      { title: '用户登陆', tap: 'login' },
+      { title: '校验用户信息', tap: 'check' },
+      { title: '获取用户加密信息', tap: 'decrypted' },
+      { title: '模板消息', tap: 'tplMessage' },
+      { title: '微信支付', tap: 'wxPay' }
+    ],
   },
 
   /**
@@ -29,6 +39,35 @@ Page({
       })
     }
     console.log("userInfo=>",this.data.userInfo)
+  },
+  // 封装模态框
+  showModal(title,content,callback){
+    wx.showModal({
+      title: title,
+      content: content,
+      showCancel: true,
+      cancelColor: '#FF6666',
+      confirmColor: '#7F8389',
+      success: function (res) { 
+        if(res.confirm){
+          callback && callback();//短路运算，如果callback为true，才执行后面的
+        }
+      }
+    })
+  },
+  clearCache(){
+    this.showModal('清除缓存','确定要清除本地数据缓存吗？',function(){
+      wx.clearStorage({
+        success:function(){
+          wx.showToast({
+            title: '清除成功',
+            icon: 'success',
+            duration: 1000,
+            mask:true
+          })
+        }
+      })
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
